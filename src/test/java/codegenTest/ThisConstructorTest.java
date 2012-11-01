cab4541937ded7cbb8c7bae630d4c6305b8b4bc7
VISTALL <vistall@i.ua>
2012-11-01 13:43:54 +0200

    this constructor tests

---
+++ src/test/java/codegenTest/ThisConstructorTest.java
@@ -0,0 +1,16 @@
+package codegenTest;
+
+import org.junit.Test;
+
+/**
+ * @author VISTALL
+ * @date 13:42/01.11.12
+ */
+public class ThisConstructorTest extends MainTest
+{
+	@Test
+	public void test()
+	{
+		run("thisConstructor.ThisConstructorTest");
+	}
+}
