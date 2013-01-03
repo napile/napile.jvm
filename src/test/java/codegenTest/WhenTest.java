c3b20a3c4b42f340bb082bfc07d0add90e7c1172
VISTALL <vistall.valeriy@gmail.com>
2013-01-03 17:26:47 +0200

    when tests

---
+++ src/test/java/codegenTest/WhenTest.java
@@ -0,0 +1,34 @@
+package codegenTest;
+
+import org.junit.Test;
+
+/**
+ * @author VISTALL
+ * @date 16:46/03.01.13
+ */
+public class WhenTest extends MainTest
+{
+	@Test
+	public void test()
+	{
+		run("whenTest.WhenTest");
+	}
+
+	@Test
+	public void test1()
+	{
+		run("whenTest.WhenTest1");
+	}
+
+	@Test
+	public void test2()
+	{
+		run("whenTest.WhenIsTest");
+	}
+
+	@Test
+	public void test3()
+	{
+		run("whenTest.WhenExpTest");
+	}
+}
